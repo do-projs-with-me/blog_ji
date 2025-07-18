f115d460bb9d48929ebfe6efb273e97ef157bfc1
Nikhil <nikhilgupta932003@gmail.com>
2025-07-18 12:51:16 +0530

    added the auth routers

--- server/dist/server.js
+++ server/dist/server.js
@@ -1,19 +1,1 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-// import cors from "cors"
-const dotenv_1 = __importDefault(require("dotenv"));
-const app = (0, express_1.default)();
-dotenv_1.default.config();
-// app.use(cors());
-const PORT = process.env.PORT || 5000;
-app.get('/', (req, res) => {
-    res.send("hello sir lets start ts");
-    console.log("stared the project");
-});
-app.listen(PORT, () => {
-    console.log("port connected");
-});
--- server/src/routes/route.ts
+++
@@ -1,8 +0,0 @@
-import  Express  from "express";
-import { signIn } from "../controller/authcontroller";
-
-const router=Express.Router();
-router.post('/signIn',signIn);
-router.post('/signUp',signUp);
-
-export default router;
